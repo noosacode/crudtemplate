0937b47f2b8d472ecab8efcd01825125f5236d33
davidM <git@7d.au>
2026-08-06 22:54:11 +1000

    no script

--- public/js/script.js
+++
@@ -1,7 +0,0 @@
-const message = document.getElementById("message");
-
-fetch("/api/message")
-    .then(response => response.json())
-    .then(data => {
-        message.textContent = data.message;
-    });
