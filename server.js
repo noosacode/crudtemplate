b14e8bb633fa22dea12ef8b74bde661808e36c46
davidM <git@7d.au>
2026-08-09 09:37:07 +1000

    login1

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require("express");
 console.log(process.env.MONGODB_URI?.substring(0, 30));
 const mongoose = require("mongoose");
 const FrangipaniTree = require("./backend/models/FrangipaniTree");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("./backend/models/User");
 
 const app = express();
 app.use(express.json());
@@ -19,6 +22,55 @@ mongoose
   });
 
 app.use(express.static("public"));
+
+app.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create user
+    const user = new User({
+      username,
+      password: hashedPassword,
+    });
+
+    await user.save();
+    res.send("User registered");
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // find the user
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).send("Invalid username or password");
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).send("Invalid username or password");
+    }
+
+    // create JWT token
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    );
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
