458aec18ba621080f9ebe3f7664f0a619236392a
davidM <git@7d.au>
2026-08-12 21:12:12 +1000

    cleanup2 remove items files

--- backend/models/Items.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const ItemsSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-});
-
-module.exports = mongoose.model("Items", ItemsSchema);
