6c8d2c71fdd4dd74f80182bac3fb136d86448c86
davidM <git@7d.au>
2026-08-12 14:45:34 +1000

    crudtemplate first commit

---
+++ backend/connect.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+async function connectDB(uri) {
+  try {
+    await mongoose.connect(uri);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+}
+
+module.exports = connectDB;
